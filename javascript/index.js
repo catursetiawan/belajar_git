364d94c7a850febdc9458ddcb0fdfb9bd7092c6b
catursetiawan <catur.setiawan29@gmail.com>
2025-09-25 13:00:12 +0700

    menambahkan gambar dan file javascript

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+let artikel=[
+    {
+        judul:'judul_1',
+        srcGambar: 'gambar/artikel1.png',
+        textArtikel: '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nobis, optio dolore sunt facilis esse. Exercitationem id voluptatibus recusandae facilis laudantium aspernatur non molestiae iusto fugiat quisquam, error cum sapiente, voluptates eaque perferendis officiis nisi voluptate accusamus quos nulla veritatis qui facere optio distinctio. Voluptate beatae, accusantium minus ratione dolore, repellendus sunt ad dicta inventore molestiae autem, quis expedita harum eveniet quod alias magni earum optio suscipit ipsam reprehenderit quae! Blanditiis placeat error ipsum alias nostrum magni quisquam iure explicabo illum animi corporis, eos commodi at culpa voluptate. Eum necessitatibus repellendus enim minus at quod, eaque adipisci ratione delectus vitae?</p> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum at modi dolorum cumque soluta. Dignissimos, eligendi? Ex nemo tenetur magni consequatur, ut, soluta eius quibusdam eaque, voluptatem cumque culpa? Perferendis?</p>'
+    },
+    {
+        judul: 'judul_2',
+        srcGambar: 'gambar/artikel1.png',
+        textArtikel: '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum at modi dolorum cumque soluta. Dignissimos, eligendi? Ex nemo tenetur magni consequatur, ut, soluta eius quibusdam eaque, voluptatem cumque culpa? Perferendis?</p>'
+    }
+]
